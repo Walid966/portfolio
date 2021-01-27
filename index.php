dbd1e120887441a73c56d9ba054332f14a9d6cd4
Walid El Ahmadi <walid0896@gmail.com>
2021-01-27 22:04:51 +0100

    nsrc

--- index.php
+++ index.php
@@ -1,123 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
-      rel="stylesheet"
-    />
-    <script
-      src="https://kit.fontawesome.com/ba3779d8d2.js"
-      crossorigin="anonymous"
-    ></script>
-    <link rel="stylesheet" href="style.css" />
-    <title>Walid El Ahmadi | Web Developer</title>
-  </head>
 
-  <body>
-    <div class="wrap">
-      <header>
-        <div class="container">
-          <nav class="nav-bar">
-            <div class="bg-b">
-              <a class="logo" href="#main">WEA</a>
-            </div>
-            <ul class="nav">
-              <li class="nav-links">
-                <a href="#about">About</a>
-              </li>
-              <li class="vl"></li>
-              <li class="nav-links">
-                <a href="#work">Work</a>
-              </li>
-              <li class="vl"></li>
-              <li class="nav-links">
-                <a href="#contact">Contact</a>
-              </li>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/ba3779d8d2.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css" />
+  <title>Walid El Ahmadi | Web Developer</title>
+</head>
 
-              <li class="copy-right">
-                <p>&copy; 2020 Walid El Ahmadi. All rights reserved</p>
-              </li>
-            </ul>
+<body>
+  <div class="wrap">
+    <header>
+      <div class="container">
+        <nav class="nav-bar">
+          <div class="bg-b">
+            <a class="logo" href="#main">WEA</a>
+          </div>
+          <ul class="nav">
+            <li class="nav-links">
+              <a href="#about">About</a>
+            </li>
+            <li class="vl"></li>
+            <li class="nav-links">
+              <a href="#work">Work</a>
+            </li>
+            <li class="vl"></li>
+            <li class="nav-links">
+              <a href="#contact">Contact</a>
+            </li>
 
-            <div class="hamburger">
-              <div class="line1"></div>
-              <div class="line2"></div>
-              <div class="line3"></div>
-            </div>
-          </nav>
+            <li class="copy-right">
+              <p>&copy; 2020 Walid El Ahmadi. All rights reserved</p>
+            </li>
+          </ul>
+
+          <div class="hamburger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main id="main">
+      <div class="container">
+        <div class="info" id="about">
+          <h1 class="name">Hi I'm Walid</h1>
+          <p>I code stuff for the web</p>
         </div>
-      </header>
-      <main id="main">
-        <div class="container">
-          <div class="info" id="about">
-            <h1 class="name">Hi I'm Walid</h1>
-            <p>I code stuff for the web</p>
-          </div>
 
-          <div id="work">
-			<h2>Recent Work</h2>
-			<div class="flex">
-				<div class="proj-container">
-				<div class="project project-1">
-					<span class="rest-name">
-					cafe<span class="red">Lorem</span>
-					<br />
-					<span class="des">CAFE - RESTAURANT - PIZZERIA</span>
-					</span>
+        <div id="work">
+          <h2>Personal Projects</h2>
+          <div class="flex">
+            <div class="proj-container">
+              <div class="project project-1">
+                <span class="rest-name">
+                  cafe<span class="red">Lorem</span>
+                  <br />
+                  <span class="des">CAFE - RESTAURANT - PIZZERIA</span>
+                </span>
 
-					<a
-					class="proj-link"
-					href="https://cafelorem.herokuapp.com/"
-					target="_blank"
-					>
-					Visit website <i class="fas fa-external-link-alt"></i>
-					</a>
-				</div>
-				<span class="proj-name">Restaurant Web App</span>
-				</div>
+                <a class="proj-link" href="https://cafelorem.herokuapp.com/" target="_blank">
+                  Visit website <i class="fas fa-external-link-alt"></i>
+                </a>
+                <a class="proj-link" href="https://github.com/Walid966/cafe-lorem" target="_blank">
+                  Source Code <i class="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+              <span class="proj-name">Restaurant Web App</span>
+            </div>
 
-				<div class="proj-container">
-				<div class="project project-2">
-					<span class="rest-name">
-					Lorem Store
-					<br />
-					<span class="des">E-commerce</span>
-					</span>
+            <div class="proj-container">
+              <div class="project project-2">
+                <span class="rest-name">
+                  Lorem Store
+                  <br />
+                  <span class="des">E-commerce</span>
+                </span>
 
-					<a
-					class="proj-link"
-					href="https://loremstore.herokuapp.com/"
-					target="_blank"
-					>
-					Visit website <i class="fas fa-external-link-alt"></i>
-					</a>
-				</div>
-				<span class="proj-name">E-commerce Website</span>
-				</div>
-			</div>
-          </div>
-
-          <div id="contact">
-            <h2>Get in touch</h2>
-            <div>
-              <h3>Email</h3>
-              <a class="email" href="mailto:walid.elahmadi9@gmail.com"
-                >walid.elahmadi9@gmail.com</a
-              >
+                <a class="proj-link" href="https://loremstore.herokuapp.com/" target="_blank">
+                  Visit website <i class="fas fa-external-link-alt"></i>
+                </a>
+                <a class="proj-link" href="https://github.com/Walid966/lorem-store" target="_blank">
+                  Source Code <i class="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+              <span class="proj-name">E-commerce Website</span>
             </div>
           </div>
         </div>
-      </main>
 
-      <footer id="footer">
-        <div class="container">
-          <p>&copy; 2020 Walid El Ahmadi. All rights reserved</p>
+        <div id="contact">
+          <h2>Get in touch</h2>
+          <div>
+            <h3>Email</h3>
+            <a class="email" href="mailto:walid.elahmadi9@gmail.com">walid.elahmadi9@gmail.com</a>
+          </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </main>
 
-    <script src="app.js"></script>
-  </body>
+    <footer id="footer">
+      <div class="container">
+        <p>&copy; 2020 Walid El Ahmadi. All rights reserved</p>
+      </div>
+    </footer>
+  </div>
+
+  <script src="app.js"></script>
+</body>
+
 </html>
