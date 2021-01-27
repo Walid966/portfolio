d01e68451e07e8b741e50f15a1bdda5b21f8c06c
Walid El Ahmadi <walid0896@gmail.com>
2021-01-27 20:53:41 +0100

    t

---
+++ pdo.php
@@ -0,0 +1,13 @@
+ <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    //try {
+    $pdo = new PDO("mysql:host=$servername;dbname=portfolio_form", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo "Connected successfully";
+    //} catch (PDOException $e) {
+    //    echo "Connection failed: " . $e->getMessage();
+    //}
