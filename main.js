3a0d7c4f13188fdb953fe7f47162158659dafe29
Walid <walid0896@gmail.com>
2021-03-20 17:03:38 +0100

    .

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-$(document).ready(function () {
-	// Add smooth scrolling to all links
-	$("a").on("click", function (event) {
-		if (this.hash !== "") {
-			event.preventDefault();
-			var hash = this.hash;
-			$("html, body").animate(
-				{
-					scrollTop: $(hash).offset().top,
-				},
-				800,
-				function () {
-					window.location.hash = hash;
-				}
-			);
-		}
-	});
-});
-
 const nav = document.querySelector("header");
 const navBar = document.querySelector(".nav");
 const hamburger = document.querySelector(".hamburger");
